6e7e0bc4f1367d470d54820cc8821f2b16d160b7
Steve W <36206062+scubastevew@users.noreply.github.com>
2022-01-06 12:01:16 -0800

    Update app.R

--- sub/app.R
+++ sub/app.R
@@ -13,7 +13,7 @@ library(shiny)
 ui <- fluidPage(
 
     # Application title
-    titlePanel("Old Faithful Geyser Data - Sub Dir - Cloned - part deux"),
+    titlePanel("Old Faithful Geyser Data rProfile"),
 
     # Sidebar with a slider input for number of bins 
     sidebarLayout(
